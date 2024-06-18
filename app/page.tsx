e019e1111d526c6673fb00e558f52a106ef27a68
Pemba Sherpa <psherpa5121@conestogac.on.ca>
2024-06-17 20:11:52 -0400

    feat: disable online from env

--- app/page.tsx
+++ app/page.tsx
@@ -23,29 +23,32 @@ export default function Home() {
               </span>
             </h2>
           </Link>
-          <Link
-            href="/create"
-            className="group rounded-lg border border-transparent px-4 py-2 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          >
-            <h2 className="mb-3 text-2xl font-semibold">
-              Create Room{" "}
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-              </span>
-            </h2>
-          </Link>
-          <Link
-            href="/join"
-            className="group rounded-lg border border-transparent px-4 py-2 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          >
-            <h2 className="mb-3 text-2xl font-semibold">
-              Join Room{" "}
-              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                -&gt;
-              </span>
-            </h2>
-          </Link>
-        
+          {process.env.NEXT_PUBLIC_ENABLE_ONLINE === "true" && 
+          <>
+            <Link
+              href="/create"
+              className="group rounded-lg border border-transparent px-4 py-2 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            >
+              <h2 className="mb-3 text-2xl font-semibold">
+                Create Room{" "}
+                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                  -&gt;
+                </span>
+              </h2>
+            </Link>
+            <Link
+              href="/join"
+              className="group rounded-lg border border-transparent px-4 py-2 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+            >
+              <h2 className="mb-3 text-2xl font-semibold">
+                Join Room{" "}
+                <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                  -&gt;
+                </span>
+              </h2>
+            </Link>
+          </>}
+
         </div>        
       </div>
     </main>
